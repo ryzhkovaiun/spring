580a1da7ba8ca3ac8747b3aeeab53cdd6c25cca4
ryzhkovaiun <yulya.mixamixa1998.ryzhkova@mail.ru>
2024-07-12 14:29:15 +0300

    added context bank

---
+++ SpringContextBank/src/main/java/bank/TransferByPhoneApp.java
@@ -0,0 +1,14 @@
+package bank;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransferByPhoneApp {
+
+    public TransferByPhoneApp() {}
+
+    public void transfer(int sourceUserId, int destinationUserId, int amount) {
+
+    }
+
+}
---
+++ SpringContextBank/src/main/java/repositories/Database.java
@@ -0,0 +1,12 @@
+package repositories;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Database {
+
+    public void transferMoney(int sourceUserId, int destinationUserId, int amount) {
+
+    }
+
+}
